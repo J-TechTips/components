929ceb83fbaedf5dea01afd686952c456ba833d2
Jonathan <jtecktips@gmail.com>
2018-06-01 14:45:54 -0700

    Understanding Built In Directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .Online
+    {
+      color: white;
+      font-size: 30px;
+    }
+    .standard
+    {
+      color: white;
+      font-size: 24x;
+    }
+  `]
 })
 export class ServerComponent {
   serverId: Number = 10;
-  serverStatus: String = 'offline';
+  serverStatus: String;
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+  getColor () {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,30 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: '<app-server></app-server>',
+  selector: 'app-servers', // element selector
+  // selector: '[app-servers]', // attribute selector
+  // selector: '.app-servers', // class selector
+  templateUrl: './servers.component.html', // external template
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `, // inline template
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
-  serverCreationStatus: string = 'No Server was created';
+  serverCreationStatus: string = 'No Server was created!';
   serverName: string;
+  serverCreated: boolean = false;
+  server = ['testserver', 'testserver2', 'testserver3'];
+
   constructor() {
     console.log(this.allowNewServer);
     setTimeout(() => {
-
-    }
-
-    )
+      this.allowNewServer = true;
+      console.log(this.allowNewServer);
+    }, 2000);
   }
 
   ngOnInit() {
   }
-  onCreateSerer() {
-    this.serverCreationStatus = 'New Server was created!' + this.serverName;
+
+  onCreateServer() {
+    this.serverCreationStatus = 'New Server was created! Name is ' + this.serverName;
   }
+
   onUpdateServerName(event: Event) {
-    console.log(event);
+    // console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
   }
+
 }
